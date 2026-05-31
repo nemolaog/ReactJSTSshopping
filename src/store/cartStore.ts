8c2ba68d4924334bccc44df0d7c92812e2d44aa0
倪墨涵 <nimohan@NemoMac.local>
2026-05-31 13:31:36 +0800

    add persist cart state from zustand with localStorage

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -1,5 +1,6 @@
-import { create } from 'zustand';
-import { Product } from '../types';
+import {create} from 'zustand';
+import {Product} from '../types';
+import {persist} from 'zustand/middleware';
 
 export interface CartItem extends Product {
     quantity: number;
@@ -31,70 +32,75 @@ function calculateCartTotals(items: CartItem[]) {
         totalAmount,
     }
 }
+
 /**
  * Global cart store
  * Manages cart items, quantities, and derived cart totals.
  */
-export const useCartStore = create<CartState>((set) => ({
-    items: [],
-    totalItems: 0,
-    totalAmount: 0,
+export const useCartStore = create<CartState>()(
+    persist(
+        (set) => ({
+            items: [],
+            totalItems: 0,
+            totalAmount: 0,
 
-    addItem: (product) => set((state) => {
-        const existingItem = state.items.find((item) => item.id === product.id)
+            addItem: (product) => set((state) => {
+                const existingItem = state.items.find((item) => item.id === product.id)
 
-        let updatedItems: CartItem[];
+                let updatedItems: CartItem[];
 
-        if (existingItem) {
-            // If the product already exists in the cart, increase its quantity.
-            updatedItems = state.items.map((item) => item.id === product.id ? {
-                    ...item, quantity: item.quantity + 1
-                } : item
-            )
-        } else {
-            // If the product is new, add it to the cart with quantity 1.
-            updatedItems = [
-                ...state.items,
-                {
-                    ...product,
-                    quantity: 1,
-                },
-            ];
-        }
-        const totals = calculateCartTotals(updatedItems);
-        return {
-            items: updatedItems,
-            ...totals,
-        };
-    }),
+                if (existingItem) {
+                    // If the product already exists in the cart, increase its quantity.
+                    updatedItems = state.items.map((item) => item.id === product.id ? {
+                            ...item, quantity: item.quantity + 1
+                        } : item
+                    )
+                } else {
+                    // If the product is new, add it to the cart with quantity 1.
+                    updatedItems = [
+                        ...state.items,
+                        {
+                            ...product,
+                            quantity: 1,
+                        },
+                    ];
+                }
+                const totals = calculateCartTotals(updatedItems);
+                return {
+                    items: updatedItems,
+                    ...totals,
+                };
+            }),
 
-    removeItem: (productId) => set((state) => {
-        const updatedItems = state.items.filter(
-            (item) => item.id !== productId
-        );
-        const totals = calculateCartTotals(updatedItems);
-        return {
-            items: updatedItems,
-            ...totals,
-        };
-    }),
-    updateQuantity: (productId, quantity) => set((state) => {
-        const updatedItems = state.items.map((item) => item.id === productId ? {
-            ...item, quantity
-        } : item).filter((item) => item.quantity > 0)
+            removeItem: (productId) => set((state) => {
+                const updatedItems = state.items.filter(
+                    (item) => item.id !== productId
+                );
+                const totals = calculateCartTotals(updatedItems);
+                return {
+                    items: updatedItems,
+                    ...totals,
+                };
+            }),
+            updateQuantity: (productId, quantity) => set((state) => {
+                const updatedItems = state.items.map((item) => item.id === productId ? {
+                    ...item, quantity
+                } : item).filter((item) => item.quantity > 0)
 
-        const totals = calculateCartTotals(updatedItems);
-        return {
-            items: updatedItems,
-            ...totals,
-        };
-    }),
-    clearCart: () => ({
-        items: [],
-        totalItems: 0,
-        totalAmount: 0,
-    }),
-}))
+                const totals = calculateCartTotals(updatedItems);
+                return {
+                    items: updatedItems,
+                    ...totals,
+                };
+            }),
+            clearCart: () => ({
+                items: [],
+                totalItems: 0,
+                totalAmount: 0,
+            }),
+        }),
+        {name: "product-explorer-cart",}
+    ))
 
 
 
